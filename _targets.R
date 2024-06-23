23e19d2e4ef8c5e2b7ca724ff5002ae73bd0ce4d
Rob Hyndman <robjhyndman@gmail.com>
2024-06-23 23:25:29 +1000

    Finished on plane

--- _targets.R
+++ _targets.R
@@ -17,40 +17,37 @@ list(
   tar_target(
     name = paradigm1_plot,
     command = pbs |>
-     filter(ATC2 == "A12", Month <= yearmonth("2006 Feb")) |>
-     autoplot() +
+      filter(ATC2 == "A12", Month <= yearmonth("2006 Feb")) |>
+      autoplot() +
       theme_void()
   ),
   tar_target(
     name = paradigm1,
-    command = savepng(paradigm1_plot, "paradigm1.png"),
-    format = "file"
+    command = savepng(paradigm1_plot, "paradigm1.png")
   ),
   tar_target(
     name = paradigm2_plot,
     command = fr_mortality |>
-      filter(Age == 20, Sex == "Male") |>
+      filter(Age == 25, Sex == "Male") |>
       autoplot(Mortality) +
       theme_void()
   ),
   tar_target(
     name = paradigm2,
-    command = savepng(paradigm2_plot, "paradigm2.png"),
-    format = "file"
+    command = savepng(paradigm2_plot, "paradigm2.png")
   ),
   tar_target(
     name = paradigm3_plot,
     command = fr_mortality |>
-      filter(Age %in% c(2:6,60), Sex == "Female") |>
+      filter(Age %in% c(2:6, 60), Sex == "Female") |>
       autoplot(Mortality) +
       guides(col = "none") +
-      theme_void()  +
+      theme_void() +
       scale_y_log10()
   ),
   tar_target(
     name = paradigm3,
-    command = savepng(paradigm3_plot, "paradigm3.png"),
-    format = "file"
+    command = savepng(paradigm3_plot, "paradigm3.png")
   ),
   # Generic graphs explaining the idea
   tar_target( # One-step forecast distribution
@@ -80,7 +77,10 @@ list(
         title = "Anomaly score density"
       ) +
       geom_vline(xintercept = u, linetype = "dashed") +
-      annotate("text", x = u + 0.1, y = 0.15, label = "90% quantile", vjust = 1, hjust = 0) +
+      annotate("text",
+        x = u + 0.1, y = 0.15,
+        label = "90% quantile", vjust = 1, hjust = 0
+      ) +
       ggfont
   ),
   tar_target( # Exceedance distribution about 95% quantile
@@ -96,23 +96,21 @@ list(
         title = "Anomaly score exceedance density"
       ) +
       geom_vline(xintercept = u, linetype = "dashed") +
-      scale_x_continuous(breaks = u, labels = "u") + theme(axis.text.x = element_text()) +
+      scale_x_continuous(breaks = u, labels = "u") +
+      theme(axis.text.x = element_text()) +
       ggfont
   ),
   tar_target(
     name = n01,
-    command = savepng(p1, "n01.png"),
-    format = "file"
+    command = savepng(p1, "n01.png")
   ),
   tar_target(
     name = as,
-    command = savepng(p2, "as.png"),
-    format = "file"
+    command = savepng(p2, "as.png")
   ),
   tar_target(
     name = ase,
-    command = savepng(p3, "ase.png"),
-    format = "file"
+    command = savepng(p3, "ase.png")
   ),
   tar_target(
     name = tscvplot,
@@ -139,6 +137,46 @@ list(
       ggfont
   ),
   tar_target(
+    name = a12,
+    command = pbs |> filter(
+      ATC2 == "A12",
+      Month <= yearmonth("2006 Jan")
+    )
+  ),
+  tar_target(
+    name = a12plus,
+    command = pbs |> filter(
+      ATC2 == "A12",
+      Month <= yearmonth("2006 Feb")
+    )
+  ),
+  tar_target(
+    name = fc_a12,
+    command = a12 |> model(ets = ETS(Scripts)) |> forecast(h = 1)
+  ),
+  tar_target(
+    name = a12plot1,
+    command = fc_a12 |>
+      autoplot(a12) +
+      labs(
+        y = "Scripts (thousands)",
+        title = "Forecast of A12 scripts: Feb 2006"
+      ) +
+      theme(legend.position = "none") +
+      ylim(35, 145)
+  ),
+  tar_target(
+    name = a12plot2,
+    command = fc_a12 |>
+      autoplot(a12plus) +
+      labs(
+        y = "Scripts (thousands)",
+        title = "Forecast of A12 scripts: Feb 2006"
+      ) +
+      theme(legend.position = "none") +
+      ylim(35, 145)
+  ),
+  tar_target(
     name = pbs_stretch,
     command = stretch_tsibble(pbs, .step = 1, .init = 36)
   ),
@@ -156,8 +194,8 @@ list(
       left_join(pbs |> rename(actual = Scripts), by = c("ATC2", "Month")) |>
       group_by(.id) |>
       mutate(
-        s = -log_likelihood(Scripts, actual), # Density scores
-        prob = lookout(density_scores = s, threshold = 0.9) # Probability not an anomaly
+        s = -log_likelihood(Scripts, actual),
+        prob = lookout(density_scores = s, threshold = 0.9)
       ) |>
       ungroup()
   ),
@@ -188,15 +226,26 @@ list(
       select(Year, Age, Sex, Mortality)
   ),
   tar_target(
+    name = fr_mortality_time_plots,
+    command = fr_mortality |>
+      ggplot(aes(x = Year, y = Mortality,
+                 color = as.factor(Age), group = Age)) +
+      geom_line() +
+      facet_grid(. ~ Sex) +
+      scale_y_log10() +
+      theme(legend.position = "none")
+  ),
+  tar_target(
     name = fr_fit,
-    command = fr_mortality |> model(arima = ARIMA(log(Mortality)))
+    command = fr_mortality |>
+      model(stl = STL(log(Mortality)))
   ),
   tar_target(
     name = fr_sigma,
     command = augment(fr_fit) |>
       as_tibble() |>
       group_by(Age, Sex) |>
-      summarise(sigma = sqrt(mean(.innov^2, na.rm = TRUE)), .groups = "drop")
+      summarise(sigma = IQR(.innov) / 1.349, .groups = "drop")
   ),
   tar_target(
     name = fr_scores,
@@ -204,43 +253,14 @@ list(
       as_tibble() |>
       left_join(fr_sigma) |>
       mutate(
-        s = -log(dnorm(.innov / sigma)), # Density scores
-        prob = lookout(density_scores = s, threshold_probability = 0.9) # Probability not an anomaly
+        s = -log(dnorm(.innov / sigma)),
+        prob = lookout(density_scores = s, threshold_probability = 0.9)
       ) |>
       select(-.model, -.resid, -.fitted, -sigma)
   ),
   tar_target(
-    name = fr_mortality2,
-    command = fr_mortality |>
-      left_join(fr_scores |> select(Year, Age, Sex, prob)) |>
-      mutate(
-        Mortality = if_else(prob < 0.05, NA_real_, Mortality)
-      )
-  ),
-  tar_target(
-    name = fr_fit2,
-    command = fr_mortality2 |> model(arima = ARIMA(log(Mortality)))
-  ),
-  tar_target(
-    name = fr_sigma2,
-    command = augment(fr_fit2) |>
-      as_tibble() |>
-      group_by(Age, Sex) |>
-      summarise(sigma = sqrt(mean(.innov^2, na.rm = TRUE)), .groups = "drop")
-  ),
-  tar_target(
-    name = fr_scores2,
-    command = augment(fr_fit2) |>
-      as_tibble() |>
-      left_join(fr_sigma2) |>
-      mutate(
-        s = -log(dnorm(.innov / sigma)), # Density scores
-        prob = lookout(density_scores = s, threshold = 0.9) # Probability not an anomaly
-      )
-  ),
-  tar_target(
     name = fr_anomalies,
-    command = fr_scores2 |>
+    command = fr_scores |>
       filter(prob < 0.05) |>
       as_tibble() |>
       select(Year, Sex, Age) |>
@@ -259,7 +279,10 @@ list(
       filter(Sex == "Male") |>
       ggplot(aes(x = Year, y = Age)) +
       facet_grid(. ~ Sex) +
-      scale_x_continuous(breaks = seq(1820, 2000, by = 20), limits = range(yrs$Year)) +
+      scale_x_continuous(
+        breaks = seq(1820, 2000, by = 20),
+        limits = range(yrs$Year)
+      ) +
       geom_point(col = "#478cb2") +
       ylim(4, 85)
   ),
@@ -269,14 +292,21 @@ list(
       filter(Sex == "Female") |>
       ggplot(aes(x = Year, y = Age)) +
       facet_grid(. ~ Sex) +
-      scale_x_continuous(breaks = seq(1820, 2000, by = 20), limits = range(yrs$Year)) +
+      scale_x_continuous(
+        breaks = seq(1820, 2000, by = 20),
+        limits = range(yrs$Year)
+      ) +
       labs(title = "French mortality anomalies") +
       geom_point(col = "#c1653a") +
       ylim(4, 85)
   ),
   tar_target(
     name = fr_anomalies_plot,
-    command = patchwork::wrap_plots(fr_anomalies_plot_female, fr_anomalies_plot_male, nrow=1)
+    command = patchwork::wrap_plots(
+      fr_anomalies_plot_female,
+      fr_anomalies_plot_male,
+      nrow = 1
+    )
   ),
   tar_target(
     name = fr_anomalies_plot_male2,
@@ -284,11 +314,19 @@ list(
       filter(Sex == "Male") |>
       ggplot(aes(x = Year, y = Age)) +
       facet_grid(. ~ Sex) +
-      scale_x_continuous(breaks = seq(1820, 2000, by = 20), limits = range(yrs$Year)) +
-      geom_vline(xintercept = unique(yrs$Year[yrs$Sex == "Male"]), alpha = 0.5, color = "grey") +
+      scale_x_continuous(
+        breaks = seq(1820, 2000, by = 20),
+        limits = range(yrs$Year)
+      ) +
+      geom_vline(
+        xintercept = unique(yrs$Year[yrs$Sex == "Male"]),
+        alpha = 0.5, color = "grey"
+      ) +
       geom_point(col = "#478cb2") +
-      ggrepel::geom_text_repel(data = yrs[yrs$Sex == "Male" & !(yrs$Year %in% 1944:1946),],
-          aes(y = 75, label = Year), col = "#478cb2", size = 3) +
+      ggrepel::geom_text_repel(
+        data = yrs[yrs$Sex == "Male", ],
+        aes(y = 75, label = Year), col = "#478cb2", size = 3, seed = 1967
+      ) +
       ylim(4, 85)
   ),
   tar_target(
@@ -297,17 +335,51 @@ list(
       filter(Sex == "Female") |>
       ggplot(aes(x = Year, y = Age)) +
       facet_grid(. ~ Sex) +
-      scale_x_continuous(breaks = seq(1820, 2000, by = 20), limits = range(yrs$Year)) +
-      geom_vline(xintercept = unique(yrs$Year[yrs$Sex == "Female"]), alpha = 0.5, color = "grey") +
+      scale_x_continuous(
+        breaks = seq(1820, 2000, by = 20),
+        limits = range(yrs$Year)
+      ) +
+      geom_vline(
+        xintercept = unique(yrs$Year[yrs$Sex == "Female"]),
+        alpha = 0.5, color = "grey"
+      ) +
       labs(title = "French mortality anomalies") +
       geom_point(col = "#c1653a") +
-      ggrepel::geom_text_repel(data = yrs[yrs$Sex == "Female" & !(yrs$Year %in% 1944:1946),],
-        aes(y = 75, label = Year), col = "#c1653a", size = 3) +
+      ggrepel::geom_text_repel(
+        data = yrs[yrs$Sex == "Female", ],
+        aes(y = 75, label = Year), col = "#c1653a", size = 3, seed = 1967
+      ) +
       ylim(4, 85)
   ),
   tar_target(
     name = fr_anomalies_plot2,
-    command = patchwork::wrap_plots(fr_anomalies_plot_female2, fr_anomalies_plot_male2, nrow=1)
+    command = patchwork::wrap_plots(
+      fr_anomalies_plot_female2,
+      fr_anomalies_plot_male2,
+      nrow = 1
+    )
+  ),
+  tar_target(
+    name = fr_anomalies_plot3,
+    command = patchwork::wrap_plots(
+      fr_anomalies_plot_female2 +
+        geom_hline(yintercept = 25, color = "gray"),
+      fr_anomalies_plot_male2 +
+        geom_hline(yintercept = 25, color = "gray") +
+        annotate("text", x = 1890, y = 27, label = "Age 25", family = "Fira Sans"),
+      nrow = 1
+    )
+  ),
+  tar_target(
+    name = fr_25,
+    command =
+      fr_mortality |>
+        filter(Age == 25) |>
+        ggplot(aes(x = Year, y = Mortality, color = Sex)) +
+        geom_line() +
+        facet_grid(Sex ~ ., scales = "free_y") +
+        geom_point(data = fr_anomalies |> filter(Age == 25)) +
+        labs(title = "French mortality: Age 25")
   ),
   # Slides
   tar_quarto(
